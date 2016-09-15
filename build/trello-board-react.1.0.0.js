46e14c79c607b897debdb49bb56acdfe7dea98d1
kuwood <p4nic510@gmail.com>
2016-09-15 12:37:36 -0600

    Change card, cardList and board to stateless components

--- build/trello-board-react.1.0.0.js
+++ build/trello-board-react.1.0.0.js
@@ -46,92 +46,36 @@
 
 	'use strict';
 	
-	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-	
-	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-	
-	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-	
-	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-	
 	var React = __webpack_require__(1);
 	var ReactDOM = __webpack_require__(34);
 	
-	var Card = function (_React$Component) {
-	    _inherits(Card, _React$Component);
+	var Card = function Card() {
+	    return React.createElement(
+	        'div',
+	        null,
+	        'This is a Card'
+	    );
+	};
 	
-	    function Card(props) {
-	        _classCallCheck(this, Card);
+	var CardList = function CardList() {
+	    return React.createElement(
+	        'div',
+	        null,
+	        React.createElement(Card, null),
+	        React.createElement(Card, null),
+	        React.createElement(Card, null)
+	    );
+	};
 	
-	        var _this = _possibleConstructorReturn(this, (Card.__proto__ || Object.getPrototypeOf(Card)).call(this, props));
-	
-	        console.log(props);
-	        return _this;
-	    }
-	
-	    _createClass(Card, [{
-	        key: 'render',
-	        value: function render() {
-	            return React.createElement(
-	                'div',
-	                null,
-	                'This is a Card'
-	            );
-	        }
-	    }]);
-	
-	    return Card;
-	}(React.Component);
-	
-	var CardList = function (_React$Component2) {
-	    _inherits(CardList, _React$Component2);
-	
-	    function CardList(props) {
-	        _classCallCheck(this, CardList);
-	
-	        return _possibleConstructorReturn(this, (CardList.__proto__ || Object.getPrototypeOf(CardList)).call(this, props));
-	    }
-	
-	    _createClass(CardList, [{
-	        key: 'render',
-	        value: function render() {
-	            return React.createElement(
-	                'div',
-	                null,
-	                React.createElement(Card, null),
-	                React.createElement(Card, null),
-	                React.createElement(Card, null)
-	            );
-	        }
-	    }]);
-	
-	    return CardList;
-	}(React.Component);
-	
-	var Board = function (_React$Component3) {
-	    _inherits(Board, _React$Component3);
-	
-	    function Board(props) {
-	        _classCallCheck(this, Board);
-	
-	        return _possibleConstructorReturn(this, (Board.__proto__ || Object.getPrototypeOf(Board)).call(this, props));
-	    }
-	
-	    _createClass(Board, [{
-	        key: 'render',
-	        value: function render() {
-	            return React.createElement(
-	                'div',
-	                null,
-	                React.createElement(CardList, null),
-	                React.createElement(CardList, null),
-	                React.createElement(CardList, null)
-	            );
-	        }
-	    }]);
-	
-	    return Board;
-	}(React.Component);
+	var Board = function Board() {
+	    return React.createElement(
+	        'div',
+	        null,
+	        React.createElement(CardList, null),
+	        React.createElement(CardList, null),
+	        React.createElement(CardList, null)
+	    );
+	};
 	
 	document.addEventListener('DOMContentLoaded', function () {
 	    ReactDOM.render(React.createElement(Board, null), document.getElementById('app'));
--- index.js
+++ index.js
@@ -1,42 +1,26 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 
-class Card extends React.Component{
-    constructor(props) {
-        super(props);
-        console.log(props);
-    }
-    render() {
+const Card = () => {
         return ( <div>This is a Card</div> );
-    }
 }
 
-class CardList extends React.Component{
-    constructor(props) {
-        super(props)
-    }
-    render() {
-        return (<div>
-                    <Card />
-                    <Card />
-                    <Card />
-                </div>
-        )
-    }
+const CardList = () => {
+    return (<div>
+                <Card />
+                <Card />
+                <Card />
+            </div>
+    )
 }
 
-class Board extends React.Component {
-    constructor(props) {
-        super(props)
-    }
-    render() {
-        return (<div>
-                    <CardList />
-                    <CardList />
-                    <CardList />
-                </div>
-                )
-    }
+const Board = () => {
+    return (<div>
+                <CardList />
+                <CardList />
+                <CardList />
+            </div>
+    )
 }
 
 document.addEventListener('DOMContentLoaded', () => {
